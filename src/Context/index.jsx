ee5cf1b36ec21ade26c84d57e6c029ba13ecc2dc
Sebastian España Alvarado <espanaalvarados@gmail.com>
2024-06-08 13:32:07 -0500

    context ok

---
+++ src/Context/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createContext, useState } from "react";
+
+export const InformationContext = createContext();
+
+export const InformationProvider = ({children}) => {
+
+    const [name, setName] = React.useState();
+    const [creditCardNumber, setCreditCardNumber] = React.useState();
+    const [expirationDate, setExpirationDate] = React.useState();
+    const [securityNumber, setSecurityNumber] = React.useState();
+
+    return(
+        <InformationContext.Provider value={{
+            name,
+            setName,
+            creditCardNumber,
+            setCreditCardNumber,
+            expirationDate,
+            setExpirationDate,
+            securityNumber,
+            setSecurityNumber
+        }} >
+            {children}
+
+        </InformationContext.Provider>
+    )
+
+};
